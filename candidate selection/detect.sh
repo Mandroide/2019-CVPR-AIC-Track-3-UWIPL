7ea0801d87f00016d9d0d39695ecaae7460cc4fa
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2021-06-12 20:18:00 -0400

    Update detect.sh

--- candidate selection/detect.sh
+++ candidate selection/detect.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+#
+# Use darknet with yolov3 to perform detections on directory of videos.
 
-for i in {6..6}
-do
-
-	cp "data/aic19-track3-test-data/$i.avi" ./
-	mkdir "data/track3_test_bg_detection/$i"
-	./darknet detector demo visdrone.data yolov3-voc.cfg yolov3-voc-25310.backup "$i.avi" -thresh 0.80
-
+readonly videos="$1/*.avi $1/*.mp4"
+for i in ${videos}; do
+  if [ -f "${i}" ]; then
+    IFS="/"
+    read -ra parts <<< "$i" # Split path
+    i=${parts[-1]} # Read just the filename
+    mkdir "data/track4_test_bg_detection/${i:0:-3}" # Make this directory with just the filename stem
+    ./darknet detector demo cfg/visdrone.data cfg/yolov3-voc.cfg yolov3-voc-25310.backup "${i}" -thresh 0.80 \
+    -out "data/track4_test_bg_detection/${i:0:-3}/${i}"
+  else
+    echo "Not video found in directory." >&2
+    exit 1
+  fi
 done
